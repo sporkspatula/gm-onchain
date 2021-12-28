0c880666bcc3f295b5251e4c062b6eb0001fda7f
Breck Stodghill <b@ourzora.com>
2021-12-28 12:50:16 -0500

    add two data contracts + update deploy script

--- deploy/01_courier_font.ts
+++ deploy/01_courier_font.ts
@@ -1,13 +1,17 @@
 module.exports = async ({ getNamedAccounts, deployments}: any) => {
     const { deploy } = deployments;
-    const { deployer, corruptionsBase } = await getNamedAccounts();
+    const { deployer, courierFont } = await getNamedAccounts();
 
-    if (!corruptionsBase) {
-        await deploy("CourierFont", {
+    if (!courierFont) {
+        const deployResult = await deploy("CourierFont", {
             from: deployer,
             args: [],
             log: true,
         });
+        console.log(deployResult)
+        // console.log(
+        //     `contract Courier Font deployed at ${deployResult.contract.address} using ${deployResult.receipt.gasUsed} gas`
+        // );
     }
 };
 module.exports.tags = ["CourierFont"];
