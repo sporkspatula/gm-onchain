f0091d05e6e917b90e57dcc5e57490bf893a678e
Breck Stodghill <b@ourzora.com>
2021-12-28 20:41:50 -0500

    hsl + deployment

--- contracts/GmRenderer.sol
+++ contracts/GmRenderer.sol
@@ -47,14 +47,37 @@ contract GmRenderer {
         return (gmData.imageName, inflated);
     }
 
+    function _getHsl(bytes32 seed) pure internal returns (Hsl memory) {
+        uint32 hue1 = uint32(bytes4(seed)) % 360;
+        uint32 hue2 = uint32(bytes4(seed << 32)) % 360;
+        uint32 s1 = (uint32(bytes4(seed << 64)) % 89) + 22;
+        uint32 s2 = (uint32(bytes4(seed << 96)) % 98) + 22;
+
+        uint32 high = (uint32(bytes4(seed << 128))) % 2;
+
+
+        uint32 lightnessAddend = ((uint32(bytes4(seed << 128))) % 20);
+
+        uint32 lightness1;
+        if (high == 0) {
+            lightness1 = 20 + lightnessAddend;
+        } else {
+            lightness1 = 60 + lightnessAddend;
+        }
+
+
+        uint32 lightness2 = 100 - lightness1;
+        return Hsl(hue1, s1, lightness1, hue2, s2, lightness2);
+    }
+
     function svgRaw(uint256 tokenId, bytes32 seed)
         external
         view
         returns (bytes memory, bytes memory)
     {
 
-        uint32 lightness1 = (uint32(bytes4(seed << 128))) % 100;
-        uint32 style = uint32(bytes4(seed << 160)) % 69;
+        Hsl memory hsl = _getHsl(seed);
+        uint32 style = uint32(bytes4(seed << 192)) % 69;
 
         bytes memory inner;
         bytes memory name;
@@ -63,8 +86,6 @@ contract GmRenderer {
         } else {
             (name, inner) = decompress(gmData2.getSvg(style));
         }
-
-        Hsl memory hsl = Hsl(uint32(bytes4(seed)) % 360, (uint32(bytes4(seed << 64)) % 89) + 22, lightness1, uint32(bytes4(seed << 32)) % 360, (uint32(bytes4(seed << 96)) % 98) + 22, 100 -lightness1);
 
         return (
             abi.encodePacked(
@@ -76,51 +97,23 @@ contract GmRenderer {
         );
     }
 
-    function toHtmlHexString(uint256 value)
-        internal
-        pure
-        returns (string memory)
-    {
-        bytes memory buffer = new bytes(7);
-        buffer[0] = "#";
-        for (uint256 i = 6; i > 0; --i) {
-            buffer[i] = _HEX_SYMBOLS[value & 0xf];
-            value >>= 4;
-        }
-
-        require(value == 0, "Strings: hex length insufficient");
-        return string(buffer);
-    }
-
     function svgPreambleString(Hsl memory hsl)
         private
         view
         returns (bytes memory)
     {
 
+        string memory hsl1 = string(abi.encodePacked('hsl(', StringsUpgradeable.toString(hsl.h1), ',', StringsUpgradeable.toString(hsl.s1), '%,', StringsUpgradeable.toString(hsl.l1), '%)'));
+        string memory hsl2 = string(abi.encodePacked('hsl(', StringsUpgradeable.toString(hsl.h2), ',', StringsUpgradeable.toString(hsl.s2), '%,', StringsUpgradeable.toString(hsl.l2), '%)'));
 
         return
             abi.encodePacked(
-                "<svg viewBox='0 0 640 640' xmlns='http://www.w3.org/2000/svg'>",
-                '<style> @font-face { font-family: CourierFont; src: url("',
+                "<svg viewBox='0 0 640 640' xmlns='http://www.w3.org/2000/svg'><style> @font-face { font-family: CourierFont; src: url('",
                 font.font(),
-                '") format("opentype"); } ',
-                ".base{fill: hsl(",
-                StringsUpgradeable.toString(hsl.h1),
-                ",",
-                StringsUpgradeable.toString(hsl.s1),
-                "%,",
-                StringsUpgradeable.toString(hsl.l1),
-                ")",
-                ";font-family:CourierFont;font-size: 16px;} ",
-                "</style>",
-                '<rect width="100%" height="100%" fill=hsl(',
-                StringsUpgradeable.toString(hsl.h2),
-                ",",
-                StringsUpgradeable.toString(hsl.s2),
-                "%,",
-                StringsUpgradeable.toString(hsl.l2),
-                ")",
+                "') format('opentype'); } .base{fill:",
+                hsl1,
+                ';font-family:CourierFont;font-size: 16px;}</style><rect width="100%" height="100%" fill="',
+                hsl2,
                 '" /> '
             );
     }
